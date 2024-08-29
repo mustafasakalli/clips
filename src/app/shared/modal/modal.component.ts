3a93dcde1e67a9d76823f971eb58eaf56874fa5e
mustafasakalli <mustafasakalli42@gmail.com>
2024-08-29 14:21:04 +0300

    add close modal functionality

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -4,14 +4,14 @@ import { ModalService } from 'src/app/services/modal.service';
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  styleUrls: ['./modal.component.css'],
 })
 export class ModalComponent implements OnInit {
+  constructor(public modal: ModalService) {}
 
-  constructor(public modal: ModalService) {
+  ngOnInit(): void {}
+
+  closeModal() {
+    this.modal.toggleModal();
   }
-
-  ngOnInit(): void {
-  }
-
 }
